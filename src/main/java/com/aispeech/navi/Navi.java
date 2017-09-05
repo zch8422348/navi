9068161640f31964041c7b26c3c03cc1735ac4d1
chunhua.zeng <chunhua.zeng@aispeech.com>
2017-09-05 12:32:25 +0800

    first commit

---
+++ src/main/java/com/aispeech/navi/Navi.java
@@ -0,0 +1,15 @@
+package com.aispeech.navi;
+
+
+import android.util.Log;
+
+/**
+ * @author Spring  09.05 2017
+ */
+
+public class Navi {
+
+    public static void init(){
+        Log.e("Navi", "i am navi");
+    }
+}
